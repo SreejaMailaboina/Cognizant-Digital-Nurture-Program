c0e295de05e0a4fce9880fce3a8bf100253135b2
SreejaMailaboina <sreejamailaboina42@gmail.com>
2026-06-03 12:01:01 +0530

    Add files via upload

---
+++ Mailaboina Sreeja_6840793/CORE_JAVA_Module3/JavaModules/com.greetings/com/greetings/GreetingApp.java
@@ -0,0 +1,12 @@
+package com.greetings;
+
+import com.utils.MessageUtil;
+
+public class GreetingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                MessageUtil.getMessage());
+    }
+}
---
+++ Mailaboina Sreeja_6840793/CORE_JAVA_Module3/JavaModules/com.utils/com/utils/MessageUtil.java
@@ -0,0 +1,8 @@
+package com.utils;
+
+public class MessageUtil {
+
+    public static String getMessage() {
+        return "Hello from Utility Module!";
+    }
+}
